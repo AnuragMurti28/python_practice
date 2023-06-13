7c1b3670c83bca91087adeddc583b5cd2587b75b
AnuragMurti28 <anuragmurti28@gmail.com>
2023-06-13 18:09:41 +0530

    writing first python code

---
+++ babypython.py
@@ -0,0 +1,5 @@
+print("First python program")
+A = "Anurag"
+input("Enter your Name")
+B=input("Enter Surname")
+print(A+B)                                                                                                                                                  
