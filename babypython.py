735436af493c0bc8093353924c1277cfcccc652c
AnuragMurti28 <anuragmurti28@gmail.com>
2023-06-14 01:28:12 +0530

    Integer, float, strings checked

--- babypython.py
+++ babypython.py
@@ -1,4 +1,22 @@
 print("First python program")
-A="Anurag"
-B=input("Enter Surname\n")
-print(A+B)                                                                                                                                              
+A = "Anurag"
+B = input("Enter Surname")
+print(A+B)
+print("\n")
+print("Number Variables\n")
+Myint = 1
+print(Myint, "\n")
+print("Floating point\n")
+Myfloat = 7.0
+print(Myfloat, "\n")
+Myfloat = float(7)
+print(Myfloat, "\n")
+String1 = "hello"
+String2 = 'yellow'
+print(String1,"\n")
+print(String2,"\n")
+mystring = "Don't worry about apostrophes"
+print("\n",mystring)
+mystring2 = 'Dont take about apostrophes'
+print(mystring2)
+
